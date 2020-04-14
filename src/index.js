41acd39e0286cf3d47b4cc9b8542acf76487b0e5
John Edward <john.escuyos@98labs.com>
2020-04-14 23:10:46 +0800

    initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
